181abcde9e233c726d03e68965c313bf4e5e0469
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2024-04-05 07:12:13 +0300

    imported the pages and their route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Homecards from './components/Homecards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from "./components/ViewAllJobs"
-const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero title="Become a React Dev" subtitle=" Find the React job that fits your skills and needs" />
-      <Homecards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom"
+import HomePage from './pages/HomePage';
+import MainLayouts from "./layouts/MainLayouts"
+import JobsPage from "./pages/JobsPage"
+import NotFound from "./pages/NotFound"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+
+    <Route path='/' element={<MainLayouts />}>
+      <Route index element={<HomePage />} /> 
+      <Route path='/jobs' element={<JobsPage />} /> 
+      <Route path='*' element={<NotFound />} /> 
+
+    </Route >
+
   )
+);
+const App = () => {
+  return <RouterProvider router={router} />
 }
 
 export default App
