cd15f2e1281aaf0b56a4aaa6e20875a4623942c4
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2024-04-05 07:11:35 +0300

    imported react-router and integrated to the app

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import Jobs from '../jobs.json'
 import JobListing from "./JobListing"
-import { useState } from "react"
-const JobListings = () => {
-    const recentJobs = Jobs.slice(0, 3)
-    
-    const {showFullDescription, setshowFullDescription} = useState(false)
+const JobListings = ({ isHome = false}) => {
+    const JobListings = isHome ? Jobs.slice(0, 3) : Jobs
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                    Browse Jobs
+                    { isHome ? "Recent Jobs" : "Browse Jobs" }
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {recentJobs.map((job) => (
+                    {JobListings.map((job) => (
                         <JobListing key={job.id} job={job} />
                     ))}
                 </div>
