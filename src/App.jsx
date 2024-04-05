1caa497791c4cc213b1e8b306cf5cca732fac6b3
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2024-04-05 08:23:16 +0300

    passed the loader data as props to the jobPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import HomePage from './pages/HomePage';
 import MainLayouts from "./layouts/MainLayouts"
 import JobsPage from "./pages/JobsPage"
 import NotFound from "./pages/NotFound"
-
+import JobPage, {jobLoader} from "./pages/JobPage"
 const router = createBrowserRouter(
   createRoutesFromElements(
 
     <Route path='/' element={<MainLayouts />}>
       <Route index element={<HomePage />} /> 
       <Route path='/jobs' element={<JobsPage />} /> 
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader}/> 
+
       <Route path='*' element={<NotFound />} /> 
 
     </Route >
