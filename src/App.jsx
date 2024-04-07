79bc10d4a2518de56f6944df09dc70379bc494bf
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2024-04-07 10:15:29 +0300

    finished the api request for add and delete job lisitng

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,44 @@ import HomePage from './pages/HomePage';
 import MainLayouts from "./layouts/MainLayouts"
 import JobsPage from "./pages/JobsPage"
 import NotFound from "./pages/NotFound"
-import JobPage, {jobLoader} from "./pages/JobPage"
-const router = createBrowserRouter(
-  createRoutesFromElements(
+import JobPage, { jobLoader } from "./pages/JobPage"
+import AddJobPage from './pages/AddJobPage';
 
-    <Route path='/' element={<MainLayouts />}>
-      <Route index element={<HomePage />} /> 
-      <Route path='/jobs' element={<JobsPage />} /> 
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader}/> 
 
-      <Route path='*' element={<NotFound />} /> 
-
-    </Route >
-
-  )
-);
 const App = () => {
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    })
+    return;
+  }
+
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: "DELETE", 
+    })
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+
+      <Route path='/' element={<MainLayouts />}>
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage deleteJob={deleteJob} />} />
+        <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+
+        <Route path='*' element={<NotFound />} />
+
+      </Route >
+
+    )
+  );
+
   return <RouterProvider router={router} />
 }
 
--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import NavBar from "../components/Navbar"
+import { ToastContainer } from 'react-toastify'
 const MainLayouts = () => {
   return (
     <>
